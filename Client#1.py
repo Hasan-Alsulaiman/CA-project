73075f388a6dec711018a87e2d3ddd4186aa7f1f
Hasan-Ali <careerforma@gmail.com>
2020-05-09 18:26:30 +0300

    closing socket successfully if user chose to

--- Client#1.py
+++ Client#1.py
@@ -8,6 +8,7 @@ msgType = "CERTREQ"
 # user id of another client whose cert is requested
 requested = "Client#2"
 
+
 def get_constants(prefix):
     """Create a dictionary mapping socket module
     constants to their names.
@@ -31,63 +32,81 @@ print('Type    :', types[sock.type])
 print('Protocol:', protocols[sock.proto])
 print()
 
-# recieve data from server
-data = sock.recv(1024)
-# if server asked for username
-if (pickle.loads(data) == 'Please Enter your username'):
-    print("recieved: ", pickle.loads(data))
-    # enter username
-    ans = input('username: ')
-    # send username
-    sock.sendall(pickle.dumps(ans))
-    print('sending', ans)
-
-# get public key
-file = open("Client#1PublicKey.pem", "rb")
-public = file.read()
-file.close()
-
-
-try:
-    # to send a certificate signing request
-    if(msgType == "CSR"):
-
-        # Send public key + user id + request type
-        message = {'key': public, 'name': ClientName, 'type': msgType}
-        print('sending {!r}'.format(message))
-        sock.sendall(pickle.dumps(message))
-    # to send my certificate for upload
-    elif(msgType == "CERTUP"):
-        # open the certificate file 
-        file = open(ClientName+'.sig', "rb")
-        cert = file.read()
-        file.close()
-        # Send certificate + user id + request type
-        message = {'key':cert, 'name': ClientName, 'type': msgType}
-        print('sending {!r}'.format(message))
-        sock.sendall(pickle.dumps(message))
-    # to request the certificate of a certain user
-    elif(msgType == "CERTREQ"):
-        # send user id of the user whose cert is requested
-        message = {'request':requested, 'name': ClientName, 'type': msgType}
-        print('sending {!r}'.format(message))
-        sock.sendall(pickle.dumps(message))
-
-
-    while True:
-        # recieve data from server
-        data = sock.recv(1024)
-        if (data):
+# recieve any initial data from server
+while True:
+    data = sock.recv(1024)
+    if(data):
+        response = pickle.loads(data)
+        # if server asked for username
+        if ( response== 'Please Enter your username'):
             print("recieved: ", pickle.loads(data))
-        # ask the client whether he wants to continue
-        ans = input('\nTerminate connection? (y/n) :')
-        # prompt user to close the connection
-        if ans == 'n':
-            continue
-        else:
+            # enter username
+            ans = input('username: ')
+            # send username
+            sock.sendall(pickle.dumps(ans))
+            print('sending', ans)
+        # if server fails to find user
+        s = 'Username not found!, if you want to sign up send "register: <username>", else send "c" to close connection'
+        if (response ==s ):
+            print(s)
+            ans2 = input('response: ')
+            sock.sendall(pickle.dumps(ans2))
+            # if user chose to close connection
+            if(ans2 == 'c'):
+                sock.close()
+                print('closing socket')
+                break
+        if(response=='Welcome'):
             break
+    else:
+        break
+
+if (response =='Welcome'):
+            # get public key
+    file = open("Client#1PublicKey.pem", "rb")
+    public = file.read()
+    file.close()
 
 
-finally:
-    print('closing socket')
-    sock.close()
+    try:
+        # to send a certificate signing request
+        if(msgType == "CSR"):
+
+            # Send public key + user id + request type
+            message = {'key': public, 'name': ClientName, 'type': msgType}
+            print('sending {!r}'.format(message))
+            sock.sendall(pickle.dumps(message))
+        # to send my certificate for upload
+        elif(msgType == "CERTUP"):
+            # open the certificate file
+            file = open(ClientName+'.sig', "rb")
+            cert = file.read()
+            file.close()
+            # Send certificate + user id + request type
+            message = {'key': cert, 'name': ClientName, 'type': msgType}
+            print('sending {!r}'.format(message))
+            sock.sendall(pickle.dumps(message))
+        # to request the certificate of a certain user
+        elif(msgType == "CERTREQ"):
+            # send user id of the user whose cert is requested
+            message = {'request': requested, 'name': ClientName, 'type': msgType}
+            print('sending {!r}'.format(message))
+            sock.sendall(pickle.dumps(message))
+
+        while True:
+            # recieve data from server
+            data = sock.recv(1024)
+            if (data):
+                print("recieved: ", pickle.loads(data))
+            # ask the client whether he wants to continue
+            ans = input('\nTerminate connection? (y/n) :')
+            # prompt user to close the connection
+            if ans == 'n':
+                continue
+            else:
+                break
+
+
+    finally:
+        print('closing socket')
+        sock.close()
