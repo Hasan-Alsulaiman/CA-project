3dc1aa0ab96f7d99d65a1c865c9aaff8b7991005
Hasan-Ali <careerforma@gmail.com>
2020-05-10 21:47:56 +0300

    it works with username

--- AGU.py
+++ AGU.py
@@ -12,7 +12,7 @@ def verify_password(username, provided_password):
     with open ('UserList.json','r') as f:
         users = json.load(f)
         f.close()
-    stored_password = users[username]['password']
+    stored_password = users[username]["password"]
     """Verify a stored password against one provided by user"""
     salt = stored_password[:64]
     stored_password = stored_password[64:]
@@ -24,6 +24,7 @@ def verify_password(username, provided_password):
     return pwdhash == stored_password
 
 
-C = hash_password("hi")
-print(C)
-V = verify_password("alli","hi")
+# C = hash_password("hi")
+
+# V = verify_password("alli","hi")
+# print(V)
