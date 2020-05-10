988bb53f5532871e0497ae85ef251bfaec4ebe39
Hasan-Ali <careerforma@gmail.com>
2020-05-10 18:10:40 +0300

    json in verify

--- verifyIt.py
+++ verifyIt.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives.serialization import load_pem_private_key
 from cryptography.hazmat.primitives.serialization import load_pem_public_key  
 import cryptography.exceptions
 import base64
-import pickle
+import json
 
 # this function takes CA's public K + user Public K + signature and verifies it
 keypath = ''
@@ -42,8 +42,8 @@ def verifyMe(keypath,sigPath,PublicKey):
 def verifyUser(ans):
 
     # open the user list
-    with open ('UserList.json','rb') as f:
-        users = pickle.loads(f)
+    with open ('UserList.json','r') as f:
+        users = json.loads(f)
         f.close()
     # check for that particular user
     if(ans in users):
