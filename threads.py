5d850ffa054bfd5466eed9d950f7e81a131239f0
Hasan-Ali <careerforma@gmail.com>
2020-05-28 22:36:34 +0300

    before calling send from recieve

--- threads.py
+++ threads.py
@@ -9,40 +9,50 @@ import keysharing
 def reciever(server,myname):
     while True:
         server.listen(1)
+        print("listening for incoming connections...")
         clientsock, clientAddress = server.accept()
-        print(clientsock, clientAddress,"connected")
-        peerdata = clientsock.recv(1024)
-        if(peerdata):
-            print("incoming msg...")
-            data = pickle.loads(peerdata)
-            print("encrypted msg: ",data[0])
-            # decrypt the encrypted password using my private key
-            password = keysharing.dec(myname+"PrivateKey.pem",b"myPassword",data[2])
-            decmsg = aes.dec(data[0],password,data[1])
-            print("msg: ",decmsg)
-        else:
-            break
-        break
+        print(clientAddress,"connected")
+        while True:
+            peerdata = clientsock.recv(1024)
+            if(peerdata):
+                print("incoming msg...")
+                data = pickle.loads(peerdata)
+                print("encrypted msg: ",data[0])
+                # decrypt the encrypted password using my private key
+                password = keysharing.dec(myname+"PrivateKey.pem",b"myPassword",data[2])
+                decmsg = aes.dec(data[0],password,data[1])
+                print("msg: ",decmsg)
+            else:
+                print("peer disconnected")
+                clientsock.close()
+                break
+            
 
 
 # thread for sending
 def sender(target,targetname):
     # Create a TCP/IP socket
     sock = socket.create_connection(('localhost', target))
-    msg = input("Enter your messege: ")
-    password = input("please choose a password: ")
-    # encrypt the msg with the password
-    cyphertext, iv = aes.enc(msg,password)
-    # encrypt the password using target user's public key
-    encpassword = keysharing.enc(targetname+"PublicKey.pem",password)
-    # put the msg in serial mode for sending
-    msgtobesent = pickle.dumps([cyphertext,iv,encpassword])
-    sock.sendall(msgtobesent)
-    ex = input("terminate connection?<y>/<n>")
-    if(ex=='y'):
-        print("closing socket..")
-        sock.close()
-        return
+    while True:
+        msg = input("Enter your messege: ")
+        password = input("please choose a password: ")
+        # encrypt the msg with the password
+        cyphertext, iv = aes.enc(msg,password)
+        # encrypt the password using target user's public key
+        encpassword = keysharing.enc(targetname+"PublicKey.pem",password)
+        # put the msg in serial mode for sending
+        msgtobesent = pickle.dumps([cyphertext,iv,encpassword])
+        sock.sendall(msgtobesent)
+        ex = input("terminate connection?<y>/<n>")
+        if(ex=='y'):
+            print("closing socket..")
+            sock.close()
+            print("closed socket..")
+            return
+            print("after return..")
+        elif(ex=='n'):
+            continue
+    print("still in thread..")
 def communication(myname,myport,targetname,targetport):
     LOCALHOST = "127.0.0.1"
     PORT = myport
@@ -52,11 +62,18 @@ def communication(myname,myport,targetname,targetport):
     # thread to handle listening
     R = threading.Thread(target=reciever, args=(server,myname))
     R.start()
-    print("listening for incoming connections...")
     # thread to send
     S = threading.Thread(target=sender, args=(targetport,targetname))
     i = input("would you like to send a msg?<y>/<n> ")
     if (i =='y'):
         S.start()
-    
+        print("at S.start()")
+        S.join()
+        print("at S.join()")
+        
+
+    R.join()
+    print("at R.join()")
+
+    return
 # communication(11000,12000)
